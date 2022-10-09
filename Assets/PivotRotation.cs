cd65a44abac88c98c4a484ab9cf50557b9936d8b
omarkhan03 <omarahmadkhan2003@gmail.com>
2022-10-09 10:21:03 -0600

    Initial commit

---
+++ Assets/PivotRotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotRotation : MonoBehaviour
+{
+    public float degreesPerSecond = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKey("a")) {
+            transform.Rotate(new Vector3(0, degreesPerSecond, 0) * Time.deltaTime);
+        }
+        if ( Input.GetKey("d")) {
+            transform.Rotate(new Vector3(0, -degreesPerSecond, 0) * Time.deltaTime);
+        }    
+        }   
+}
