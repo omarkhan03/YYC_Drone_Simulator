d00d0d74948a5f082ab7a17cef40451188d02049
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-01-05 16:22:47 -0700

    fpv indicator

--- Assets/Code/Scripts/FPV.cs
+++ Assets/Code/Scripts/FPV.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPV : MonoBehaviour
 {
@@ -8,26 +9,22 @@ public class FPV : MonoBehaviour
     public Rigidbody drone;
     bool fpv = false;
     public Vector3 offset = new Vector3(0, (float)1.5, -2);
+    public Text indicator;
 
     // Update is called once per frame
     void Update()
     {
-
-/*
-        if (!fpv) {
-            transform.position = drone.position + offset;
-        } else {
-            transform.position = drone.position;
-        }
-        */
-
         if (Input.GetKeyDown("f"))
         {
             fpv = !fpv;
             if (fpv) {
                 transform.localPosition -= offset;
+                indicator.text = "FPV ON (f to toggle)";
+                indicator.color = new Color(0,(float)0.5,0);
             } else {
                 transform.localPosition += offset;
+                indicator.text = "FPV OFF (f to toggle)";
+                indicator.color = new Color((float)0.5,0,0);
             }
         }
     }
